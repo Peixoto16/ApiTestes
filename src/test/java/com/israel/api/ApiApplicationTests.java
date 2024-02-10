08276d3984c7c950b05efc59a54641c15e35b774
Israel Peixoto <israel1peixoto@gmail.com>
2024-02-10 03:32:01 -0300

    ADD

---
+++ src/test/java/com/israel/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.israel.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
